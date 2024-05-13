d93d11b006f50c70c4c2f00f4c9f35225eb48348
bmallar <benmallar@gmail.com>
2024-05-13 12:39:30 -0500

    add files from other repo

---
+++ src/content_option.js
@@ -0,0 +1,144 @@
+const logotext = "Ben Mallar";
+const meta = {
+    title: "Bens Portfolio",
+    description: "I’m Ben Mallar data scientist _ Full stack devloper, looking for work",
+};
+
+const introdata = {
+    title: "I’m Ben Mallar",
+    animated: {
+        first: "I am a Software Developer and Data Scientist",
+        second: "I code front-end and back-end for websites",
+        third: "I develop fun and interavtive mobile apps ",
+    },
+    description: "Welcome to my page I am a aspiring software developer",
+    your_img_url: "https://i.ibb.co/m49b5cc/Screenshot-5.png",
+};
+
+const dataabout = {
+    title: "A little about me",
+    aboutme: "My name is Ben Mallar I am 23 years old. To get into coding I enrolled in a 12 week fast paced and intense full stack web development bootcamp at Northwestern. Devoting myself to this bootcamp made me fluent in many coding languages, it also made my problem solving and debugging skills prominent. Initally my goal for learning code was to get a high paying job, but every week of endless hours put in, I got more and more hooked. I started really enjoying it, while learning it at the same time, it made me realize more and more daily thingd people use is coded. I want to help make peoples lives easier with new and improved user friendly UX. I genuinely enjoy doing this and getting to work with others who think the same would be a dream.",
+};
+const worktimeline = [{
+        jobtitle: "I am looking for someone to help me get my foot in the door",
+        where: "I can work remote or anywhere in the Panama City, Florida area",
+        
+    },
+ 
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Database Management",
+        value: 80,
+    },
+    {
+        name: "Node",
+        value: 60,
+    },
+    {
+        name: "Api Integration",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I am able to create or enhance UX for any app or website. There is always inprovements to be made to your code, whether its style or functionality.",
+    },
+    {
+        title: "Website & Database Creation",
+        description: "I can make a fully functioning website or database from scratch. I can use any language and database you prefer.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I can create you a mobile app. It can be a brand new idea or I can convert your website into a clean user friendly app.",
+    },
+];
+
+const dataportfolio = [{
+  
+        img: "https://i.ibb.co/z5gbrT8/quiz-page-screenshot.jpg",
+        description: "Trivl a is ten question trivia game with functionality mataching popular app, Wordle. This displays thorough comprehenion of Javascript and phone app creation",
+        link: "https://njohnson2897.github.io/trivl-project/",
+    },
+    {
+        img: "https://i.ibb.co/19LytFY/activ-landing-page-screenshot.jpg",
+        description: "Activ is a workout tracking and sharing application that allows users to track past workouts, view other users’ workouts for inspiration, and share accomplishments to their friends",
+        link: "https://activ.onrender.com/",
+    },
+    {
+        img: "https://i.ibb.co/7j5ZKBz/Screenshot-37.png",
+        description: "SVG logo generator that uses iquirer prompts to build a logo, also having tests to maintain my code easier.",
+        link: "https://drive.google.com/file/d/1-UPC9yKsnIiLxzx-S__dyvfBCIKZxjKV/view",
+    },
+    {
+        img: "https://i.ibb.co/p0qQM5x/Screenshot-39.png",
+        description: "Ventrilo 2 is a texting app made that updates in live time, built with socket.io and coded using the full MERN stack",
+        link: "https://p3-mern-chat.onrender.com/",
+    },
+    {
+        img: "https://i.ibb.co/bsSy2mj/Screenshot-36.png",
+        description: "Readme genenrator uses inquirer to make a prefect readme by following inquirer prompts.",
+        link: "https://drive.google.com/file/d/18b5zajMlXSa1bqINpZ31olMabh7pIPYZ/view",
+    },
+
+    {
+        img: "https://i.ibb.co/7vLwJxd/Screenshot-28.png",
+        description: "I made a Ai prompt generator that answers any coding questions you have. This shows off my understanding of Ai Promt Engineering, Javascript, Node.js and Api integration",
+        link: "https://github.com/bmallar/Ai-Project",
+    },
+    {
+        img: "https://i.ibb.co/SxfxQHh/Screenshot-29.png",
+        description: "Employee Tracker Backend shows I can create a well organized SQL that's made with Node.js, inquirer and postgres",
+        link: "https://drive.google.com/file/d/1KX4SvbxbGGPrgvJToMTCYzKD0hbYz6kh/view",
+    },
+    {
+        img: "https://i.ibb.co/87k9q1x/Screenshot-27.png",
+        description: "JATE Text Editor is downloadable, offline note taking app showing off my ability to create servive workers, and use webpack",
+        link: "https://pwa-texteditor-m4c4.onrender.com/",
+    },
+   
+    {
+        img: "https://i.ibb.co/Qfjfx1P/Screenshot-3.png",
+        description: "Horiseon web page shows I can create a clean simple web page using HTML, CSS",
+        link: "https://bmallar.github.io/Horiseon-Refactor/",
+    },
+ 
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "benmallar@gmail.com",
+    YOUR_FONE: "(815)501-5666",
+    description: "You can reach out to me with any questions via phone, text or email ",
+   
+    YOUR_SERVICE_ID: "service_is2v9xi",
+    YOUR_TEMPLATE_ID: "template_3vehi53",
+    YOUR_USER_ID: "QoFrNel-NwhRj2kwk",
+};
+
+const socialprofils = {
+    github: "https://github.com/bmallar",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/ben-mallar-aa08ba304?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BJeyWynqLTSWVj6EK7d16hg%3D%3D",
+    twitter: "https://twitter.com/MallarBen",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
